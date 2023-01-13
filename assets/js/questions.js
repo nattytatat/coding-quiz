ee3c1269d7bef839a8dc91c401139383e1c59831
Natalie <natalie@bigorangemedia.co.uk>
2023-01-13 18:35:45 +0000

    add js files and create questions

---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+var quizQuestions = [
+    {
+    question: "What is your name?",
+    choices: ["My name is not your business", "My name is Sir Lancelot of Camelot", "I. Am. Dave.", "I am Boris Johnson"],
+    correct: "My name is Sir Lancelot of Camelot"
+},
+    {
+    question: "What is your quest?",
+    choices: ["To seek the Holy Grail", "To buy a new curved Monitor", "To learn Javascript", "To avoid Internet Explorer"],
+    correct: "To seek the Holy Grail"
+},
+    {
+    question: "What is your favourite colour?",
+    choices: ["I'm afraid I'm colour blind", "I am quite partial to pink", "Blue", "Yellow"],
+    correct: "Blue"
+},
+    {
+    question: "What is the airspeed velocity of an unladen swallow?",
+    choices: ["I say old chap, are you quoting a movie?", "24 miles per hour", "Bird speed cannot be measured", "The speed of sound"],
+    correct: "24 miles per hour"
+}
+];
