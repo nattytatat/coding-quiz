48d9d415dcf979a33d084e51078c0923dbfadc65
Natalie <natalie@bigorangemedia.co.uk>
2023-01-13 20:26:40 +0000

    change some query selectors, restart for loop on questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,25 +1,42 @@
 // Place where score will be displayed
-var finalScore = document.getElementById("final-score");
-var startBtn = document.getElementById("start");
-var startScreen = document.getElementById("start-screen");
-var questionSection = document.getElementsByClassName("hide");
+var finalScore = document.querySelector("#final-score");
+var startBtn = document.querySelector("#start");
+var startScreen = document.querySelector("#start-screen");
+var questionSection = document.querySelector(".hide");
+var questionTitle = document.querySelector("#question-title");
+var theChoices = document.querySelector("#choices")
 
-// keep up to date with the score - may need to start with the total time
-var theScore = 0;
+//time left
+var timeLeft = 60;
+// keep up to date with the score
+var finalScore = 0;
+// keep an eye on the current question to ensure starts at the first and the game ends on the last
+var currentQuestion = 0;
 
 // run the game
 function theGame() {
     console.log("button has been clicked");
-    
-    // remove the starter text
-    startScreen.style.display = "none";
-    // display the questions in a for loop
-    questionSection[0].classList.remove("hide");
+
+    // loop through array questions and choices
+    for (i = 0; i < quizQuestions.length; i++) {
+        alert(quizQuestions[i].question);
+        questionTitle.innerText = quizQuestions[i].question;
+
+    }
+
+    // add timer
+
+    // stop game when questions end
 }
+
 
 // create an event listener for the button to start the game
 startBtn.addEventListener("click", function(event) {
     event.preventDefault();
+    
+    // remove the starter text and remove hide class from question section
+    startScreen.style.display = "none";    
+    questionSection.classList.remove("hide");
 
     theGame();
     
